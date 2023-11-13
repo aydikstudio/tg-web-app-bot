f6fe07ab3b6dc3b51e336c6adb4bf8f91183f4e2
aydikstudio <aydikstudio@mail.ru>
2023-11-13 19:32:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ bot.on('message', async (msg) => {
     // });
   }
 
-  if(msg?.web_app_data?.data) {
+  if(msg.web_app_data.data) {
     try {
         const data = JSON.parse(msg?.web_app_data?.data)
   
